20c51a7ec320a4376a88891886d59b55016ae128
vovaukrainets <127029812+gitvova@users.noreply.github.com>
2023-10-15 13:34:28 +0300

    do it
    do it

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,21 @@
+class Key {
+  private signature: number;
+}
+
+getSignature(): number {
+    return signature;
+};
+
+class Person{
+    private key: Key; 
+}
+
+abstract class House{
+    door: true;
+    key: Key;
+    tenants: Person[];
+}
+
 const key = new Key();
 
 const house = new MyHouse(key);
@@ -6,6 +24,5 @@ const person = new Person(key);
 house.openDoor(person.getKey());
 
 house.comeIn(person);
-
 
 export {};
