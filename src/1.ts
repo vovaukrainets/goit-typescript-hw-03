4d8736abfe380fa01fa4b6ff1b538ab5f22d8d42
vovaukrainets <127029812+gitvova@users.noreply.github.com>
2023-10-15 09:52:53 +0300

    start
    start

--- src/1.ts
+++ src/1.ts
@@ -4,16 +4,18 @@
   напишіть скорочену ініціалізацію.
 */
 
+// class Student {
+//   public name: string;
+//   public age: number;
+//   public grade: string;
+
+//   constructor(name: string, age: number, grade: string) {
+//     this.name = name;
+//     this.age = age;
+//     this.grade = grade;
+//   }
+// }
 class Student {
-  public name: string;
-  public age: number;
-  public grade: string;
-
-  constructor(name: string, age: number, grade: string) {
-    this.name = name;
-    this.age = age;
-    this.grade = grade;
-  }
+  constructor(public name: string, public age: number, public grade: string) {}
 }
-
 export {};
